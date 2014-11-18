0da3d0882023095f6a4aac3467ccfb80cae8f6f0
cmjscott <cmjscott@gmail.com>
2014-11-18 09:52:15 -0500

    Finished and commented everything

--- IC_Nov_17/IC_Nov_17/main_IC.cpp
+++ IC_Nov_17/IC_Nov_17/main_IC.cpp
@@ -1,61 +1,64 @@
-﻿#include <iostream>
-#include <conio.h> //include for getch function
-#include <cmath> // used for pow function
-#include <iomanip> //include for setprecision command
+﻿#include <iostream> // for cin/cout
+#include <conio.h> // for getch function
+#include <cmath> // for exp function
+#include <iomanip> // include for setprecision command
 
-
+// function prototype declarations
 double trapz(double lBound, double uBound, int n);
 double funct(double x);
 
 using namespace std;
 
-
-/*Write a program that determines an estimate of the integral (between two specified points) of the 
-following equation:
-y = 4e^(−x)
-The program should contain a function that utilizes the trapezoidal rule to calculate the area under the 
-curve. The inputs to the function should be the interval endpoints and number of trapezoids (as entered 
-by the user). The main() portion of the program should not involve any calculations of area. These 
-should all be contained in your function. Your main() code should only contain statements that call for 
-inputs, call the area function, and output the results back to the user. Demonstrate the functionality of 
-your program over the interval between 0 and 1 with 5, 50, and 100 trapezoids*/
-
 int main()
 {
 	double lBound, uBound;
-	int n;
+	int n,ending(0);
 
-	// basic input stuff
-	cout << "Enter lower bound: ";
-	cin >> lBound;
-	cout << "Enter upper bound: ";
-	cin >> uBound;
-	cout << endl << "Enter number of sections: ";
-	cin >> n;
-	
-	cout << trapz(lBound, uBound, n) << endl;
+	// do while loop used for running 3 times. Tried to terminate program based on keypress, but it wasn't working and I got frustrated. 
+	// will learn and fix later.
+	do{
+		// basic input stuff
+		cout << "Enter lower bound: ";
+		cin >> lBound;
+		cout << "Enter upper bound: ";
+		cin >> uBound;
+		cout << "Enter number of sections: ";
+		cin >> n;
+
+		printf("Trapezoidal estimation of the integral 4*exp(-x) from %.2f to %.2f\nusing %i sections is: %f\n\n", lBound, uBound, n,trapz(lBound, uBound, n));
+		
+		++ending;
+	} while (ending < 3);
 
 	_getch();
-
 	return 0;
 }
 
+
+/* Performes the trapezoid rule on the given function
+ from the lower bound to the uppper bound using n trapezoids.
+ uses: integral(F(x)| blound - ubound = deltaX/2 * (F(lbound) + F(ubound) ...
+ + 2 * sum(F(lbound + i * deltaX)) from i = 1 to i = n-1
+ */
 double trapz(double lBound, double uBound, int n)
 {
-	double dx, sum(0);
-	dx = (uBound - lBound) / n;
+	double dx, sum(0); // define required variables
+	dx = (uBound - lBound) / n; //calculate step size
 
+	// sum the 2nd through penultimate function value and multiply by 2
 	for (int i = 1; i < n; ++i)
 	{
 		sum += (2 * funct(lBound + (i * dx)));
-		cout << i << endl;
+		//cout << i << endl;
 	}
-	sum += (funct(lBound) + funct(uBound));
-	sum = sum * dx / 2;
+	
+	sum += (funct(lBound) + funct(uBound));// add the first and last function value to the sum
+	sum = sum * dx / 2; // multiply entire thing by base/2 (deltaX / 2)  
 
 	return sum;
 }
 
+// because I'm lazy and didn't want to type the entire function out each time.
 double funct(double x)
 {
 	return 4 * exp(-x);
