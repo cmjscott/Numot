d4b863916c73c0adee6d1fb6c2e6b3417c86a7a3
cmjscott <cmjscott@gmail.com>
2014-11-24 15:29:00 -0500

    asdf

--- Homework_3/Homework_3/main.cpp
+++ Homework_3/Homework_3/main.cpp
@@ -5,7 +5,7 @@
 #include <conio.h> //include for getch function
 #include <iomanip> //include for setprecision command
 
-void bubble(std::vector<int>* v);
+void bubble(std::vector<int>* v, bool ascending);
 
 
 /*Write a program that sorts a series of numbers in ascending or descending order. The program should 
@@ -18,6 +18,8 @@ int main()
 {
 		std::vector<int> vec, sortedVec;
 		int inputInt;
+		char flag;
+		bool ascending;
 
 		do
 		{
@@ -26,26 +28,56 @@ int main()
 			vec.push_back(inputInt);
 		} while (vec.size() < 5);
 
-		std::sort(vec.begin(), vec.end());
-		
+		do
+		{
+			std::cout << std::endl << "Ascending (a) or Decending (d) order?\n";
+			flag = (char)_getch();
+			
+		} while (flag != 'a' && flag != 'd');
+
+		if (flag == 'a')
+		{
+			ascending = true;
+		} else {
+			ascending = false;
+		}
+
+
+		//std::sort(vec.begin(), vec.end());
+		bubble(&vec,ascending);
+
 		for (int i = 0; i < vec.size(); ++i)
 		{
 			std::cout << vec[i] << ' ';
 		}
-
 		_getch();
 	return 0;
 }
 
-void bubble(std::vector<int>* v)
+void bubble(std::vector<int>* v, bool _ascending)
 {
 	int temp;
 
 	for (int i = 0; i < v->size(); ++i)
 	{
-		//for (int j = )
-
-
-	}
-	
+		for (int j = 0; j < (v->size()- i - 1); ++j)
+		{
+			if (_ascending)
+			{
+				if ((*v)[j]>(*v)[j + 1])
+				{
+					temp = (*v)[j + 1];
+					(*v)[j + 1] = (*v)[j];
+					(*v)[j] = temp;
+				}
+			}else{
+				if ((*v)[j]<(*v)[j + 1])
+				{
+					temp = (*v)[j + 1];
+					(*v)[j + 1] = (*v)[j];
+					(*v)[j] = temp;
+				}
+			}//if (_ascending)
+		}//for (int j = 0; j < (v->size()- i - 1); ++j)
+	}//for (int i = 0; i < v->size(); ++i)
 }
