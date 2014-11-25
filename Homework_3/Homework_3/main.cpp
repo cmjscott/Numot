db9cae7cabe5a058bdd1ed5d395485f31387a0f3
cmjscott <cmjscott@gmail.com>
2014-11-25 15:19:24 -0500

    asdf

--- Homework_3/Homework_3/main.cpp
+++ Homework_3/Homework_3/main.cpp
@@ -4,6 +4,8 @@
 #include <cmath> // used for pow function
 #include <conio.h> //include for getch function
 #include <iomanip> //include for setprecision command
+#include <cctype> // for istype functions
+
 
 void bubble(std::vector<int>* v, bool ascending);
 
@@ -27,18 +29,25 @@ int main()
 			std::cout << "Please give me an integer: ";
 			std::cin >> inputInt;
 
-
-			//TODO: prevent implicit typecast to int if a double is provided to cin
-			while (std::cin.fail()) 
+			//checks if the stream encountered a fatal error (ie. a char was entered)
+			if (std::cin.fail()) 
 			{
-				std::cout << "Error, please enter an integer." << std::endl;
-				std::cin.clear();
-				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-				std::cin >> inputInt;
+				std::cin.clear(); // clears the error
 			}
 
+			// clear out any additional input from the stream until the end of the line
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
 
-			vec.push_back(inputInt);
+			// if the ignore cleared out more than one character (the null terminator)
+			// assume that bad data was passed to the input stream
+			// because otherwise the stream would be clear, since all the data was placed into inputInt
+			if (std::cin.gcount() > 1)
+			{
+				std::cout << "Error, non integer data entered" << std::endl;
+			} else {
+				vec.push_back(inputInt);
+			}
+
 		} while (vec.size() < 5);
 
 		//Asks the user if they want their vector sorted in ascending or decending order
@@ -48,6 +57,7 @@ int main()
 			flag = (char)_getch();
 		} while (flag != 'a' && flag != 'd');
 
+		// converts the flag char to a boolean
 		if (flag == 'a')
 		{
 			ascending = true;
@@ -56,11 +66,13 @@ int main()
 		}
 
 		bubble(&vec,ascending);
-		 
+		
+		std::cout << "Sorted Array:\n[ ";
 		for (int i = 0; i < vec.size(); ++i)
 		{
 			std::cout << vec[i] << ' ';
 		}
+		std::cout << "]\n";
 		_getch();
 	return 0;
 }
